2ce61a6c6fe64260b891e9b265c1db5d5fba9f9d
S.Agalya <agalyaishere@gmail.com>
2024-05-13 16:51:40 +0530

    commit

--- vercel/src/pages/index.js
+++ vercel/src/pages/index.js
@@ -3,6 +3,7 @@ const index = () => {
   return (
     <>
     
+    
     <div 
     style={{ display: 'flex', flexDirection:"column",justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
         <h1 className="font-bold text-size:20" >HELLO </h1><br/>
@@ -14,8 +15,19 @@ const index = () => {
           height={37}
         />
       </div>
+      <div  style={{ position: 'absolute', top: '70px',left:"20px"}}>
+      <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+            
+      </div>
+      <h1 style={{ position: 'absolute', top: '70px', left: '50%', transform: 'translateX(-50%)' ,fontSize: '2rem' }} className="font-bold ">WELCOME TO NEXT.JS</h1>
 
-      
     </>
   )
 }
