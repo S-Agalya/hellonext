08c8e0ad4854bb69cb438f3a7a6d14c8379537ac
S.Agalya <agalyaishere@gmail.com>
2024-05-14 14:09:02 +0530

    commit

--- vercel/src/pages/submit_page.js
+++ vercel/src/pages/submit_page.js
@@ -32,10 +32,10 @@ export default function ContactSubmitted() {
 
   return (
     <div className="max-w-md mx-auto my-8 p-4 bg-white shadow-md rounded-lg">
-      <h1 className="text-2xl font-bold mb-4">Contact Submitted</h1>
-      <p className="mb-2"><strong>Name:</strong> {name}</p>
-      <p className="mb-2"><strong>Email:</strong> {email}</p>
-      <p className="mb-2"><strong>Message:</strong> {message}</p>
+      <h1 className="text-2xl font-bold mb-4" style={{color:'black'}}>Contact Submitted</h1>
+      <p className="mb-2" style={{color:'black'}}><strong>Name:</strong>  {name}</p>
+      <p className="mb-2" style={{color:'black'}}><strong>Email:</strong> {email}</p>
+      <p className="mb-2" style={{color:'black'}}><strong>Message:</strong> {message}</p>
     </div>
   );
 }
