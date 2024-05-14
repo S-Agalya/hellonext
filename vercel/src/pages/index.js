948fb33227712c21514eb3b6bf458e2e12e65dda
S.Agalya <agalyaishere@gmail.com>
2024-05-15 01:07:41 +0530

    commit

--- vercel/src/pages/index.js
+++ vercel/src/pages/index.js
@@ -3,9 +3,12 @@ import ContactForm from "@/components/ContactForm"
 const index = () => {
   return (
     <>
-  
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-center font-bold bg-wgite-500">CONTACT FORM</h1>
+        
+      <div className="min-h-screen flex items-center justify-center bg-white-600">
+      
       <ContactForm/>
+      
     </div>
     </>
   )
--- vercel/src/pages/submit_page.js
+++ vercel/src/pages/submit_page.js
@@ -1,21 +1,28 @@
 import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/router'
 import React from 'react'
+
 
 const submit_page = () => {
     const searchparams=useSearchParams()
-
+  const router=useRouter()
     const name=searchparams.get( 'name')
     const email=searchparams.get('email')
     const message=searchparams.get('message')
+    function handlebackclick(){
+      router.push('/')
+    }
   return (
     <>
-    <div className='max-w-md mx-auto my-8 p-4 bg-white shadow-md rounded-lg'>
-        <p className='mb-4' style={{color:'black'}}><strong>Name:</strong>{name}</p>
-        <p className='mb-4' style={{color:'black'}}><strong>Email:</strong>{email}</p>
-        <p className='mb-4' style={{color:'black'}}><strong>Message:</strong>{message}</p>
+    <h1 className='text-center pt-8 font-bold textsize-10'>FORM SUBMITTED SUCCESSFULLY</h1>
+    <div className='max-w-md mx-auto my-8 pt-8   shadow-md rounded-lg'>
+        <p className='mb-4' style={{color:'black'}}><strong>Name: </strong>{name}</p>
+        <p className='mb-4' style={{color:'black'}}><strong>Email: </strong>{email}</p>
+        <p className='mb-4' style={{color:'black'}}><strong>Message: </strong>{message}</p>
     </div>
-    
-    
+    <div className=' flex items-center justify-center '>
+    <button type='submit' onClick={handlebackclick} className="px-4 py-2 bg-blue-500 text-white  rounded">BACK!!</button>
+    </div>
     </>
   )
 }
