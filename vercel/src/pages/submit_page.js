32c5b6f96a901f1b35835ecc3dc55546922d73e0
S.Agalya <agalyaishere@gmail.com>
2024-05-15 12:24:51 +0530

    commit

--- vercel/src/pages/submit_page.js
+++ vercel/src/pages/submit_page.js
@@ -14,14 +14,16 @@ const submit_page = () => {
     }
   return (
     <>
-    <h1 className='text-center pt-8 font-bold textsize-10'>FORM SUBMITTED SUCCESSFULLY</h1>
+    
+    <h1 className='text-center pt-8 font-bold text-2xl bg-blue-100 '><span className='pt-4'>FORM SUBMITTED SUCCESSFULLY</span></h1>
     <div className='max-w-md mx-auto my-8 pt-8   shadow-md rounded-lg'>
-        <p className='mb-4' style={{color:'black'}}><strong>Name: </strong>{name}</p>
-        <p className='mb-4' style={{color:'black'}}><strong>Email: </strong>{email}</p>
-        <p className='mb-4' style={{color:'black'}}><strong>Message: </strong>{message}</p>
+        <p className='mb-4  ' style={{color:'black'}}><strong>Name: </strong>{name}</p>
+        <p className='mb-4  ' style={{color:'black'}}><strong>Email: </strong>{email}</p>
+        <p className='mb-4 ' style={{color:'black'}}><strong>Message: </strong>{message}</p>
     </div>
-   
+  
     <div className=' flex items-center justify-center '>
+    
     <button type='submit' onClick={handlebackclick} className="px-4 py-2 bg-blue-500 text-white  rounded">BACK!!</button>
     </div>
     </>
