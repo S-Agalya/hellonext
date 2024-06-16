253be30e8ed6c2474eb8af53c35743b5086f461f
S.Agalya <agalyaishere@gmail.com>
2024-06-16 19:25:15 +0530

    commit

--- vercel/src/pages/login.jsx
+++ vercel/src/pages/login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSession, signIn, signOut } from 'next-auth/react'
 
 const Login = () => {
-    const { data: session } = useSession()
+    const session = useSession()
 
     if (session) {
         return (
