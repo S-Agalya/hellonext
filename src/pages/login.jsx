8394752491174b4c808a7109705839f897c6c1a8
S.Agalya <agalyaishere@gmail.com>
2024-06-19 13:12:53 +0530

    commit

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { useSession, signIn, signOut } from 'next-auth/react';
-
-
+import Link from 'next/link';
 const Login = () => {
     const { data: session } = useSession();
-    
+   
     if (session) {
         return (
             <>
@@ -28,12 +27,16 @@ const Login = () => {
                             <input type="password" placeholder="Password" className='mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm '/>
                         </label>
                         <button className='btn btn-primary w-full px-6 py-2 rounded-full bg-blue-500 text-white' onClick={()=>signIn()}>Sign in</button>
+                          
+                        <div className='flex flex-col items-center justify-center   '>
+                    <p className='pr-5 mb-5 text-xl text-gray-800'> or </p>
+                    <button onClick={() => signIn()} className="btn btn-primary  px-6 py-2 rounded-full bg-blue-500 text-white pl-5"> Sign in with google</button>
+                </div>
+                        
                     </div>
                 </div>
-                <div className='flex flex-col items-center justify-center min-h-screen '>
-                    <p className='pr-5 mb-4 text-xl text-gray-800'>You are not signed in</p>
-                    <button onClick={() => signIn()} className="btn btn-primary px-6 py-2 rounded-full bg-blue-500 text-white ">Sign in</button>
-                </div>
+
+                
             </>
         );
     }
