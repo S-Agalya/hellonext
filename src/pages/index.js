3372ccc5fd1f994d6596cbb2c04e1bc1144cc55c
S.Agalya <agalyaishere@gmail.com>
2024-08-03 13:00:40 +0530

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
-import ContactForm from "@/components/ContactForm"
-import Login from "./login"
+//import ContactForm from "@/components/ContactForm"
+import Login from './login'
+
 
 const index = () => {
   return (
     <>
-        
-        
       <div className="min-h-screen flex items-center justify-center bg-white-600">
-      
       <Login/>
       
     </div>
